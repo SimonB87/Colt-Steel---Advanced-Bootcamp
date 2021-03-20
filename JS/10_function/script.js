200e5a4c5ef7e34e722223ac2c79762690e728e4
SimonB87 <buryans@gmail.com>
2021-03-20 10:19:32 +0100

    try & catch in JS

--- JS/10_function/script.js
+++ JS/10_function/script.js
@@ -1,9 +1,26 @@
-function makeBetweenFunc(min, max) {
-  return function(num){
-    return num >= min && num <= max;
+// # Try and Catch
+
+// - if ther e might be an error in the code, we can catch it and execue some other code in order not to break the script
+
+// - easy way is to execute something, that doesnt exists
+
+// run code even if there was no "hello" defined. : 
+/* hello.toUpperCase(); */// returns: Uncaught ReferenceError: hello is not defined at script.js:7
+
+/*
+try {
+  hello.toUpperCase();
+} catch {
+  console.log("Error!");
+}
+*/
+
+function yell(msg){
+  try {
+    msg = `${msg} `;
+    console.log(msg.toUpperCase().repeat(3));
+  } catch(e) { // Param "e" allows us to get the Error message.
+    console.log(e);
+    console.log("Please pass in a valid string.");
   }
 }
-
-const isChild = makeBetweenFunc(0,18);
-
-isChild(10);
